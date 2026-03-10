b86cc9af5f4092a9e1f30ce0ae8c926de7ec1aba
alexiamc <alexiamcor@gmail.com>
2026-03-09 20:11:28 -0400

    feat: code cunks with more parameters and comments

--- code_chunks/raster_code_chunks.R
+++ code_chunks/raster_code_chunks.R
@@ -8,89 +8,140 @@ data <- volcano
 
 # TDA
 
-# gridDiag sublevel gudhi
+# gridDiag sublevel GUDHI
 gridDiag(
   FUNvalues     = data,                     # must use FUNvalues for raster, user must provide array (or matrix)
   maxdimension  = length(dim(data)) - 1,    # default for FUNvalues
   sublevel      = TRUE,                     # default                   
   library       = "GUDHI",                  # default
-  location      = FALSE,                    # default, when true lim and by should be set
+  location      = FALSE,                    # default
   printProgress = FALSE                     # default
 )
 
-# gridDiag sublevel dyonysus 
+# gridDiag sublevel Dionysus 
 gridDiag(
   FUNvalues    = data,
   maxdimension = length(dim(data)) - 1,
   sublevel     = TRUE,
   library      = "Dionysus",
   location      = FALSE,
-  printProgress = FALSE 
+  printProgress = FALSE
 )
 
-# gridDiag sublevel dyonysus 
+# gridDiag sublevel PHAT 
 gridDiag(
   FUNvalues    = data,
   maxdimension = length(dim(data)) - 1,
   sublevel     = TRUE,
   library      = "PHAT",
   location      = FALSE,
-  printProgress = FALSE 
+  printProgress = FALSE
 )
 
-# gridDiag superlevel gudhi
+# gridDiag superlevel GUDHI
 gridDiag(
   FUNvalues     = data,
   maxdimension  = length(dim(data)) - 1,
   sublevel      = FALSE, 
   library       = "GUDHI",
   location      = FALSE,                   
-  printProgress = FALSE 
+  printProgress = FALSE
 )
 
-# gridDiag superlevel dyonysus
+# gridDiag superlevel Dionysus
 gridDiag(
-  FUNvalues    = data,
-  maxdimension = length(dim(data)) - 1,
-  sublevel     = FALSE,
-  library      = "Dionysus",
+  FUNvalues     = data,
+  maxdimension  = length(dim(data)) - 1,
+  sublevel      = FALSE,
+  library       = "Dionysus",
   location      = FALSE,                   
-  printProgress = FALSE 
+  printProgress = FALSE
 )
 
-# gridDiag superlevel dyonysus
+# gridDiag superlevel PHAT
 gridDiag(
   FUNvalues    = data,
   maxdimension = length(dim(data)) - 1,
   sublevel     = FALSE,
   library      = "PHAT",
   location      = FALSE,
-  printProgress = FALSE 
+  printProgress = FALSE
 )
 
 # TODO: add location = true, diagLimit?
 
+# gridDiag Dionysus, location = TRUE 
+gridDiag(
+  FUNvalues     = data,
+  lim           = rbind(c(1, 1), c(nrow(data), ncol(data))), # if location = true, user must provide 
+  by            = c(1, 1),                                   # if location = true, user must provide
+  maxdimension  = length(dim(data)) - 1,
+  sublevel      = TRUE,
+  library       = "Dionysus",                                # if location = true, library must be "Dionysus" or "PHAT"
+  location      = TRUE,                                     
+  printProgress = FALSE
+)
+
+
+# gridDiag PHAT, location = TRUE 
+gridDiag(
+  FUNvalues     = data,
+  lim           = rbind(c(1, 1), c(nrow(data), ncol(data))), 
+  by            = c(1, 1),                                   
+  maxdimension  = length(dim(data)) - 1,
+  sublevel      = TRUE,
+  library       = "PHAT",                                   
+  location      = TRUE,                                     
+  printProgress = FALSE
+)
+
+# gridDiag sublevel, diagLimit
+gridDiag(
+  FUNvalues     = data,
+  maxdimension  = length(dim(data)) - 1,
+  sublevel      = TRUE,
+  library       = "GUDHI",
+  location      = FALSE,
+  printProgress = FALSE,
+  diagLimit     = max(data) + 10   # replaces Inf if sublevel = true
+)
+
+# gridDiag superlevel GUDHI, diagLimit
+gridDiag(
+  FUNvalues     = data,
+  maxdimension  = length(dim(data)) - 1,
+  sublevel      = FALSE,
+  library       = "GUDHI",
+  location      = FALSE,
+  printProgress = FALSE,
+  diagLimit     = min(data) - 10   # replaces -Inf if sublevel = false
+)
+
 
 # ripserr
 
-# cubical sublevel default *missing one point compared to TDA
+# cubical sublevel default 
 cub_sub <- cubical(
-  data,             # user must provide
-  treshold = 9999,  # default
-  method = "lj",     # default
+  data,                # user must provide matrix or array
+  threshold = 9999,    # default
+  method = "lj"        # default
   )
-as.data.frame(cub_sub) # to see the persistence diagram
+as.data.frame(cub_sub) # persistence diagram
 
 # cubical sublevel method = cp
 cub_sub_cp <- cubical(
-  data,             # user must provide
-  treshold = 9999,  # default
+  data,             
+  threshold = 9999,  
   method = "cp"    
 )
-as.data.frame(cub_sub_cp) # to see the persistence diagram
+as.data.frame(cub_sub_cp) 
 
 
-# for superlevel negate data and then negate birth and death 
-cub_sup <- cubical(-data)
-transform(as.data.frame(cub_sup), birth = -birth, death = -death) 
+# cubical superlevel 
+cub_sup <- cubical(
+  -data,            # negate data
+  threshold = 9999,
+  method = "lj"
+  )
+transform(as.data.frame(cub_sup), birth = -birth, death = -death) # negate birth and death
 
